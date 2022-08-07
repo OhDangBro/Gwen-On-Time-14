66bebc8c86a4c668fe8e8f04cde6206091419290
Will <103394466+OhDangBro@users.noreply.github.com>
2022-08-07 13:35:50 -0400

    set up models/index.js

--- models/index.js
+++ models/index.js
@@ -1,1 +1,31 @@
-a
+/// IMPORTS USER, POST, COMMENT JS FILES ///
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+// CONNECTIONS WITH KEYS //
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+// CONNECTION THROUGH FOREIGN KEYS FOR COMMENTS //
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+Users.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };
