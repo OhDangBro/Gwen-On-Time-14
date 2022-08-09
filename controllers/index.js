66694fdea65b03035ca161a0fa2d5793ede012d2
Will <103394466+OhDangBro@users.noreply.github.com>
2022-08-09 13:40:54 -0400

    updated controller/index so posts now show

--- controllers/index.js
+++ controllers/index.js
@@ -9,11 +9,51 @@ const postRoutes = require('./api/post-routes');
 // router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 
+// old get code for homepage
+// router.get('/', (req, res) => {
+//   res.render('homepage');
+// });
 
+// new get code
+router.get('/', async (req, res) => {
+  try {
+    const data = await Post.findAll({
+      include: [
+        {
+          model: Users,
+          attributes: ['user_name', 'id'],
+        },
+      ],
+    });
 
-router.get('/', (req, res) => {
-  res.render('homepage');
+    const posts = data.map((post) =>
+      post.get({ plain: true })
+    );
+    console.log('the posts:', posts); //console log ALL posts in terminal
+    res.render('homepage', {
+      posts,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const data = await Post.findByPk(req.params.id);
+
+    const post = data.get({ plain: true });
+    console.log('the post: ', post) //console log post in terminal
+    res.render('singlepost', { 
+      post 
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 
 router.get('/login', (req, res) => {
   res.render('login');
