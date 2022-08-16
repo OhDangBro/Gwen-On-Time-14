2730d02b11bb15654cbc95b815538abce43c4869
Will <103394466+OhDangBro@users.noreply.github.com>
2022-08-16 19:04:26 -0400

    made file structure look like module

--- seeds/index.js
+++ seeds/index.js
@@ -1,17 +1,26 @@
-const seedUsers = require('./user-seed');
-const seedPosts = require('./post-seed');
-const seedComments = require('./comment-seed');
-const seedVote = require('./vote-seed');
+const seedUsers = require('./user-seeds');
+const seedPosts = require('./post-seeds');
+const seedComments = require('./comment-seeds');
+const seedVotes = require('./vote-seeds');
 
 const sequelize = require('../config/connection');
 
-const seedAll = async() => {
-    await sequelize.sync({ force: true });
-    await seedUsers();
-    await seedPosts();
-    await seedComments();
-    await seedVote();
-    process.exit(0);
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('--------------');
+  await seedUsers();
+  console.log('--------------');
+
+  await seedPosts();
+  console.log('--------------');
+
+  await seedComments();
+  console.log('--------------');
+
+  await seedVotes();
+  console.log('--------------');
+
+  process.exit(0);
 };
 
 seedAll();
--- seeds/user-seed.js
+++ seeds/user-seed.js
@@ -1,25 +1,59 @@
-///// DO NOT TOUCH ////
-const { Users } = require('../models');
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
 
-const userData = [{
-        username: 'Gwen',
-        password: 'Gwen2',
-        email: 'gwen@aol.com'
-        
-
-    },
-    {
-        username: 'Jessica',
-        password: 'Gwen3',
-        email: 'Jessica@aol.com'
-    },
-    {
-        username: 'TomatoJelly',
-        password: 'Gwen4',
-        email: 'Tomato@aol.com'
-    }
+const userdata = [
+  {
+    username: 'alesmonde0',
+    email: 'nwestnedge0@cbc.ca',
+    password: 'password123'
+  },
+  {
+    username: 'jwilloughway1',
+    email: 'rmebes1@sogou.com',
+    password: 'password123'
+  },
+  {
+    username: 'iboddam2',
+    email: 'cstoneman2@last.fm',
+    password: 'password123'
+  },
+  {
+    username: 'dstanmer3',
+    email: 'ihellier3@goo.ne.jp',
+    password: 'password123'
+  },
+  {
+    username: 'djiri4',
+    email: 'gmidgley4@weather.com',
+    password: 'password123'
+  },
+  {
+    username: 'msprague5',
+    email: 'larnout5@imdb.com',
+    password: 'password123'
+  },
+  {
+    username: 'mpergens6',
+    email: 'hnapleton6@feedburner.com',
+    password: 'password123'
+  },
+  {
+    username: 'tpenniell7',
+    email: 'kperigo7@china.com.cn',
+    password: 'password123'
+  },
+  {
+    username: 'msabbins8',
+    email: 'lmongain8@google.ru',
+    password: 'password123'
+  },
+  {
+    username: 'jmacarthur9',
+    email: 'bsteen9@epa.gov',
+    password: 'password123'
+  }
 ];
 
-const seedUsers = () => Users.bulkCreate(userData);
+const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
 
 module.exports = seedUsers;
