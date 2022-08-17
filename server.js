0dc2d598efbbb5ff59ddabf2153de28a5ec053a4
Will <103394466+OhDangBro@users.noreply.github.com>
2022-08-17 13:01:35 -0400

    heroku push

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-///Notes////
+
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
